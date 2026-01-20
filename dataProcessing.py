2c763c952060331a62ede4acf005234a3db40b5c
Nikhil <nikhilagrawal6448@gmail.com>
2026-01-20 10:33:50 +0530

    setup

---
+++ dataProcessing.py
@@ -0,0 +1,41 @@
+import chromadb
+from sentence_transformers import SentenceTransformer
+client = chromadb.Client()
+
+collections = client.create_collection(name="test_collection")
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+
+def embeddingFunction(texts):
+    return model.encode(texts,normalize_embeddings=True)
+
+def addDocumentsToCollection(collection, docs, metas, ids):
+    collection.add(
+        documents=docs,
+        metadatas=metas,
+        ids=ids,
+        embeddings=embeddingFunction(docs)
+    )
+
+def basicQueryCollection(collection, query_texts, n_results):
+    query_embeddings = model.encode(query_texts, normalize_embeddings=True)
+    results = collection.query(
+        query_embeddings=query_embeddings,
+        n_results=n_results
+    )
+    return results
+
+addDocumentsToCollection(
+    collections,
+    docs=["Hello world hello how are you nice to meet you hello once again and bye"],
+    metas=[{"lang": "en"}, {"lang": "fr"}, {"lang": "es"}],
+    ids=["1"]
+)
+
+results = basicQueryCollection(
+    collections,
+    query_texts=["Hello"], 
+    n_results=2
+)
+
+print(results)
---
+++ main.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+import inngest
